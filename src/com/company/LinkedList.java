135382a71316327f43d6aaaa4d86da9eb79fa05c
Loveseo8 <58880849+Loveseo8@users.noreply.github.com>
2020-02-20 14:46:38 +0300

    Update LinkedList.java

--- src/com/company/LinkedList.java
+++ src/com/company/LinkedList.java
@@ -28,21 +28,21 @@ public class LinkedList {
         prev_node.next = new_node;
     }
 
-    public void append(int new_data){
+    public void append(int new_data) {
         Node new_node = new Node(new_data);
 
-        if(head == null){
+        if (head == null) {
             head = new Node(new_data);
             return;
         }
-        new_node.next = null;
 
+        new_node.next = null;
         Node last = head;
-        while (last.next != null){
-            last = last.next;
-            last.next = new_node;
-            return;
-        }
+
+        while (last.next != null) last = last.next;
+
+        last.next = new_node;
+        return;
     }
 
     void deleteNode(int index){
